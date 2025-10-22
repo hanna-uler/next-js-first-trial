7a30d07affa8042949ea68311d47f2e063c85dd9
hanna-uler <hannauler@gmail.com>
2025-10-22 00:04:02 -0400

    add icons, img, companies page, practice dynamic routing

---
+++ src/app/companies/[...id]/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/header";
+
+export interface PageProps{
+    params: { id: string[] };
+}
+
+// export function generateStaticParams() {
+//     return [{id:'1'}, {id:'2'}, {id:'3'}, ]
+// }
+
+export default function Page({params}:PageProps) {
+    return <>
+        <Header>Companies ({String(params.id)})</Header>
+        <p>{new Date().toTimeString()}</p>
+    </>
+}
