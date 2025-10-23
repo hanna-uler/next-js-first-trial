498d15070c5662d93d072f7f14a8e55afc3e55b8
hanna-uler <hannauler@gmail.com>
2025-10-23 12:47:04 -0400

    practicing parallel routes, add dashboard slots, imgs,fake api

--- src/app/(admin)/dashboard/newCompanies/page.tsx
+++ src/app/(admin)/dashboard/newCompanies/page.tsx
@@ -5,7 +5,8 @@ export interface PageProps { };
 export default function Page(params:PageProps) {
     return (
         <main>
-            <h1 className="text-xl">Dashboard Page</h1>
+            <h1 className="text-xl">Dashboard Page / New Companies</h1>
+            <p>Some additional info</p>
             {/* <MagicButton/> */}
         </main>
     );
